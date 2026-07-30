b37524c98fe569ca6fd119b834edfb7e46e80629
SKBWEBDEV <najmujshakib025@gmail.com>
2026-07-30 22:53:59 +0600

    Add notification delete API and improve notification management

--- ecobazer-backend/routes/notificationRoutes.js
+++ ecobazer-backend/routes/notificationRoutes.js
@@ -1,10 +1,3 @@
-const {
-  getNotifications,
-  markAsRead,
-  deleteNotification,
-} = require("../controlers/notificationController");
-
-
 const express = require("express");
 
 const router = express.Router();
@@ -12,14 +5,18 @@ const router = express.Router();
 const {
   getNotifications,
   markAsRead,
+  deleteNotification,
 } = require("../controlers/notificationController");
 
 const secureMiddleware = require("../middleware/secureMiddleware");
 
+// Get user notifications
 router.get("/", secureMiddleware, getNotifications);
 
+// Mark all notifications as read
 router.patch("/read", secureMiddleware, markAsRead);
 
+// Delete single notification
 router.delete("/:id", secureMiddleware, deleteNotification);
 
 module.exports = router;
